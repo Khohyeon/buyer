bfcf522094649216784b21688d827eb572d175ef
kim ho hyeon <khh5762@naver.com>
2023-01-18 10:19:01 +0900

    mapper 설정 user_tb,product_tb

--- src/main/java/shop/mtcoding/buyer/controller/model/PurchaseRepository.java
+++ src/main/java/shop/mtcoding/buyer/controller/model/PurchaseRepository.java
@@ -1,5 +1,22 @@
 package shop.mtcoding.buyer.controller.model;
 
-public interface PurchaseRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PurchaseRepository {
+    // Create
+    public int insert(int userId, int productId);
+
+    // Read
+    public List<Purchase> firnAll();
+
+    public Purchase findById();
+
+    // Update
+    public int updateById(int id, int userId, int productId);
+
+    // Delete
+    public int deleteById(int id);
 }
